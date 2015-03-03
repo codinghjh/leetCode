fa2cea72fb21569be8d5ca88bcff64248e2d3955
codinghjh <hanjunhua.123123@gmail.com>
2015-03-03 17:29:44 +0800

    Combination Sum

---
+++ LeetCode/src/leetCode/Combination_Sum.java
@@ -0,0 +1,35 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Combination_Sum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result=new ArrayList<>();
+        Arrays.sort(candidates);
+        List<Integer> temp=new ArrayList<Integer>();
+        for(int i=0;i<candidates.length&&target>=candidates[i];i++)
+        {
+        	if(candidates[i]==target)
+        	{
+        		temp.add(candidates[i]);
+        		if(!result.contains(temp))
+        			result.add(temp);
+        	}
+        	else
+        	{
+				for(List<Integer> aList:combinationSum(candidates, target-candidates[i]))
+				{
+					aList.add(candidates[i]);
+			        Collections.sort(aList);
+			        if(!result.contains(aList))
+			        	result.add(aList);
+			        
+				}
+			}
+        }
+        return result;
+    }
+}
