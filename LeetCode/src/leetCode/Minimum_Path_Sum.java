b61f725e63df0470ca5f154a302730fa54b87371
codinghjh <hanjunhua.123123@gmail.com>
2015-03-29 13:52:45 +0800

    Minimum Path Sum

---
+++ LeetCode/src/leetCode/Minimum_Path_Sum.java
@@ -0,0 +1,17 @@
+package leetCode;
+
+public class Minimum_Path_Sum {
+    public int minPathSum(int[][] grid) {
+        int row=grid.length,col=grid[0].length;
+        for(int i=1;i<row;i++)
+        	grid[i][0]+=grid[i-1][0];
+        for(int i=1;i<col;i++)
+        	grid[0][i]+=grid[0][i-1];
+        for(int i=1;i<row;i++)
+        	for(int j=1;j<col;j++)
+        	{
+        		grid[i][j]+=grid[i][j-1]<grid[i-1][j]?grid[i][j-1]:grid[i-1][j];
+        	}
+        return grid[row-1][col-1];
+    }
+}
